29eeebda85cd7f384b4550617c459379ca320412
alannuarii <alannuariii@gmail.com>
2024-07-10 07:29:51 +0800

    Update

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	// "log"
 	"fmt"
-	// "strconv"
+	"time"
+	"strconv"
 	// "go-api-gdrive/utils"
-	// "github.com/tealeg/xlsx"
     "github.com/360EntSecGroup-Skylar/excelize"
 )
 
@@ -26,9 +26,9 @@ func main() {
 		return
 	}
 
-	sheetName := "7" // Ganti sesuai nama sheet Anda
-	row := 7         // Ganti dengan nomor baris yang ingin dibaca
-	col := "K"       // Ganti dengan huruf kolom yang ingin dibaca
+	sheetName := "9" // Ganti sesuai nama sheet Anda
+	row := 12         // Ganti dengan nomor baris yang ingin dibaca
+	col := "H"       // Ganti dengan huruf kolom yang ingin dibaca
 
 	cellValue := f.GetCellValue(sheetName, fmt.Sprintf("%s%d", col, row))
 	if cellValue == "" {
@@ -36,6 +36,24 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Data pada sheet %s baris %d kolom %s: %s\n", sheetName, row, col, cellValue)
+	// Coba konversi nilai sel menjadi float64
+	numValue, err := strconv.ParseFloat(cellValue, 64)
+	if err != nil {
+		// Jika tidak bisa dikonversi ke float64, mungkin nilai bukan numerik atau waktu
+		fmt.Printf("Data pada sheet %s baris %d kolom %s: %s\n", sheetName, row, col, cellValue)
+		return
+	}
+
+	// Periksa apakah nilai numerik ini mungkin merupakan waktu Excel
+	if numValue > 0 && numValue < 1 {
+		// Excel menyimpan waktu sebagai bagian desimal dari hari (misalnya, 0.5 = 12:00 PM)
+		timeValue := time.Date(1899, 12, 30, 0, 0, 0, 0, time.UTC).Add(time.Duration(numValue * 24 * float64(time.Hour)))
+		formattedValue := timeValue.Format("15:04")
+		fmt.Printf("Data pada sheet %s baris %d kolom %s: %s\n", sheetName, row, col, formattedValue)
+	} else {
+		// Jika tidak, format sebagai angka biasa
+		formattedValue := fmt.Sprintf("%.2f", numValue)
+		fmt.Printf("Data pada sheet %s baris %d kolom %s: %s\n", sheetName, row, col, formattedValue)
+	}
 }
 
